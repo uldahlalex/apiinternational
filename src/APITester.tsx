e4dd4467d63319480fb74b143ac52f86c5aeba11
Alex Uldahl <alex@uldahl.dk>
2026-08-11 13:21:16 +0200

    capturing input field values using onChange(e => ....)

--- src/APITester.tsx
+++ src/APITester.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, type ChangeEvent} from "react";
 
 export interface MyRegisterForm {
     email: string;
@@ -7,25 +7,25 @@ export interface MyRegisterForm {
 
 export function APITester() {
 
-  const [registerForm, setRegisterForm] = useState<MyRegisterForm>({
-      password: '',
-      email: 'your@email.com'
-  })
+    const [registerForm, setRegisterForm] = useState<MyRegisterForm>({
+        password: '',
+        email: 'your@email.com'
+    })
 
-
-    function loadData() {
-
+    function onChangeEvent(e: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
+        setRegisterForm({...registerForm, email: e.target.value})
     }
 
-    function onChangeEvent() {
+    function sendForm() {
         
     }
 
     return (
     <div>
-        <input value={registerForm.email} onChange={onChangeEvent} />
-        <input type="password" />
-      <button onClick={() => loadData()}>click me</button>
+        <input value={registerForm.email} onChange={e => onChangeEvent(e)} />
+        <input type="password" onChange={e =>
+            setRegisterForm({...registerForm, password: e.target.value})} />
+      <button onClick={() => sendForm()}>click me</button>
  </div>
   );
 }
